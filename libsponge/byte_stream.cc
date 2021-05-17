aba70a078bc1ad6ffbe00c4a582b7d0b41d1b888
dentiny <1910821171@qq.com>
2021-05-16 22:33:27 -0400

    lab0

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -1,5 +1,7 @@
 #include "byte_stream.hh"
 
+#include <iostream>
+
 // Dummy implementation of a flow-controlled in-memory byte stream.
 
 // For Lab 0, please replace with a real implementation that passes the
@@ -12,42 +14,65 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-ByteStream::ByteStream(const size_t capacity) { DUMMY_CODE(capacity); }
+ByteStream::ByteStream(const size_t capacity) :
+  buffer_(vector<char>(capacity)),
+  capacity_(capacity),
+  read_off_(0),
+  write_off_(0),
+  buffer_size_(0),
+  total_nread_(0),
+  total_nwrite_(0),
+  input_ended_(false),
+  error_(false) {}
 
 size_t ByteStream::write(const string &data) {
-    DUMMY_CODE(data);
-    return {};
+  size_t write_size = min(remaining_capacity(), data.length());
+  if (write_off_ + write_size < capacity_) {
+    for (size_t cur_write_size = 0; cur_write_size < write_size; ++cur_write_size) {
+      buffer_[write_off_++] = data[cur_write_size];
+    }
+  } else {
+    size_t cur_write_size = 0;
+    for (; write_off_ < capacity_; ++write_off_) {
+      buffer_[write_off_] = data[cur_write_size++];
+    }
+    write_off_ = 0;
+    for (; cur_write_size < write_size; ++cur_write_size) {
+      buffer_[write_off_++] = data[cur_write_size];
+    }
+  }
+  buffer_size_ += write_size;
+  total_nwrite_ += write_size;
+  return write_size;
 }
 
 //! \param[in] len bytes will be copied from the output side of the buffer
 string ByteStream::peek_output(const size_t len) const {
-    DUMMY_CODE(len);
-    return {};
+  size_t peek_size = min(buffer_size(), len);
+  string content;
+  if (read_off_ + peek_size <= capacity_) {
+    content += string(buffer_.begin() + read_off_, buffer_.begin() + read_off_ + peek_size);
+  } else {
+    content += string(buffer_.begin() + read_off_, buffer_.end());
+    size_t already_read_size = capacity_ - read_off_;
+    content += string(buffer_.begin(), buffer_.begin() + (peek_size - already_read_size));
+  }
+  return content;
 }
 
 //! \param[in] len bytes will be removed from the output side of the buffer
-void ByteStream::pop_output(const size_t len) { DUMMY_CODE(len); }
+void ByteStream::pop_output(const size_t len) {
+  size_t pop_size = min(buffer_size(), len);
+  read_off_ = (read_off_ + pop_size) % capacity_;
+  buffer_size_ -= pop_size;
+  total_nread_ += pop_size;
+}
 
 //! Read (i.e., copy and then pop) the next "len" bytes of the stream
 //! \param[in] len bytes will be popped and returned
 //! \returns a string
-std::string ByteStream::read(const size_t len) {
-    DUMMY_CODE(len);
-    return {};
+string ByteStream::read(const size_t len) {
+  string content = peek_output(len);
+  pop_output(len);
+  return content;
 }
-
-void ByteStream::end_input() {}
-
-bool ByteStream::input_ended() const { return {}; }
-
-size_t ByteStream::buffer_size() const { return {}; }
-
-bool ByteStream::buffer_empty() const { return {}; }
-
-bool ByteStream::eof() const { return false; }
-
-size_t ByteStream::bytes_written() const { return {}; }
-
-size_t ByteStream::bytes_read() const { return {}; }
-
-size_t ByteStream::remaining_capacity() const { return {}; }
--- libsponge/byte_stream.hh
+++ libsponge/byte_stream.hh
@@ -2,6 +2,7 @@
 #define SPONGE_LIBSPONGE_BYTE_STREAM_HH
 
 #include <string>
+#include <vector>
 
 //! \brief An in-order byte stream.
 
@@ -16,8 +17,15 @@ class ByteStream {
     // all, but if any of your tests are taking longer than a second,
     // that's a sign that you probably want to keep exploring
     // different approaches.
-
-    bool _error{};  //!< Flag indicating that the stream suffered an error.
+    std::vector<char> buffer_;
+    size_t capacity_;
+    size_t read_off_;  // byte index of next read
+    size_t write_off_;  // byte index of next write
+    size_t buffer_size_;
+    size_t total_nread_;
+    size_t total_nwrite_;
+    bool input_ended_;
+    bool error_;  //!< Flag indicating that the stream suffered an error.
 
   public:
     //! Construct a stream with room for `capacity` bytes.
@@ -32,13 +40,13 @@ class ByteStream {
     size_t write(const std::string &data);
 
     //! \returns the number of additional bytes that the stream has space for
-    size_t remaining_capacity() const;
+    size_t remaining_capacity() const { return capacity_ - buffer_size_; }
 
     //! Signal that the byte stream has reached its ending
-    void end_input();
+    void end_input() { input_ended_ = true; }
 
     //! Indicate that the stream suffered an error.
-    void set_error() { _error = true; }
+    void set_error() { error_ = true; }
     //!@}
 
     //! \name "Output" interface for the reader
@@ -56,29 +64,29 @@ class ByteStream {
     std::string read(const size_t len);
 
     //! \returns `true` if the stream input has ended
-    bool input_ended() const;
+    bool input_ended() const { return input_ended_;  }
 
     //! \returns `true` if the stream has suffered an error
-    bool error() const { return _error; }
+    bool error() const { return error_; }
 
     //! \returns the maximum amount that can currently be read from the stream
-    size_t buffer_size() const;
+    size_t buffer_size() const { return buffer_size_; }
 
     //! \returns `true` if the buffer is empty
-    bool buffer_empty() const;
+    bool buffer_empty() const { return buffer_size_ == 0; }
 
     //! \returns `true` if the output has reached the ending
-    bool eof() const;
+    bool eof() const { return input_ended_ && buffer_empty(); }
     //!@}
 
     //! \name General accounting
     //!@{
 
     //! Total number of bytes written
-    size_t bytes_written() const;
+    size_t bytes_written() const { return total_nwrite_; }
 
     //! Total number of bytes popped
-    size_t bytes_read() const;
+    size_t bytes_read() const { return total_nread_; }
     //!@}
 };
 
